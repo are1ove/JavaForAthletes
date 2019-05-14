c8a0991fe12033736f21526cb42240d87141f489
Ilya <chapa00@inbox.ru>
2019-05-14 21:46:33 +0300

    Second Step to the End

--- src/Controllers/Controller.java
+++ src/Controllers/Controller.java
@@ -1,8 +1,14 @@
 package Controllers;
 
 import java.io.IOException;
-import java.net.URL;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.*;
+import java.util.Arrays;
 import java.util.ResourceBundle;
+
+import client_server.Command;
+import client_server.Status;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -34,43 +40,66 @@ public class Controller {
     private Button LoginBtn;
 
     @FXML
-    void initialize() {
+    void initialize() throws Exception {
         Func theFunc = new Func();
         LoginBtn.setOnAction(event -> {
             String loginText = LoginField.getText().trim();
             String loginPassword = PasswordField.getText().trim();
 
-            if(!loginText.equals("") && !loginPassword.equals("")) {
-                String flag = theFunc.login(loginText+" "+loginPassword);
-                if (flag == "*Вход прошел успешно*"){
-                    SignUpBtn.getScene().getWindow().hide();
 
-                    FXMLLoader loader = new FXMLLoader();
-                    loader.setLocation(getClass().getResource("../view/app.fxml"));
+            try {
+                if(!loginText.equals("") && !loginPassword.equals("")) {
+                    final Socket clientSocket = new Socket("localhost", 3248);
 
-                    try {
-                        loader.load();
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    ObjectOutputStream writer = new ObjectOutputStream(clientSocket.getOutputStream());
+                    ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
+
+                    Command command = new Command("login", Arrays.asList(loginText,loginPassword));
+                    writer.writeObject(command);
+                    writer.flush();
+                    Status status = (Status) inFromServer.readObject();
+                    String flag = status.getMessage();
+                    System.out.println(flag);
+
+                    if (status.getMessage() == "*Вход прошел успешно*"){
+                        Stage stage = (Stage) LoginBtn.getScene().getWindow();
+                        stage.close();
+
+                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/app.fxml"));
+                        Parent root1 = null;
+                        try {
+                            root1 = (Parent) fxmlLoader.load();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        stage = new Stage();
+                        stage.initModality(Modality.APPLICATION_MODAL);
+                        stage.setScene(new Scene(root1));
+                        stage.show();
                     }
-
-                    Parent root = loader.getRoot();
-                    Stage stage = new Stage();
-                    stage.setScene(new Scene(root));
-                    stage.showAndWait();
+                    else{
+                        Alert alert1 = new Alert(Alert.AlertType.ERROR, "Неправильный логин или пароль");
+                        alert1.getDialogPane().getChildren().stream().filter(node -> node instanceof Label).forEach(node -> ((Label)node).setMinHeight(Region.USE_PREF_SIZE));
+                        alert1.show();
+                    }
                 }
                 else{
-                    Alert alert1 = new Alert(Alert.AlertType.ERROR, "Неправильный логин или пароль");
-                    alert1.getDialogPane().getChildren().stream().filter(node -> node instanceof Label).forEach(node -> ((Label)node).setMinHeight(Region.USE_PREF_SIZE));
-                    alert1.show();
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Вы не ввели логин или пароль");
+                    alert.getDialogPane().getChildren().stream().filter(node -> node instanceof Label).forEach(node -> ((Label)node).setMinHeight(Region.USE_PREF_SIZE));
+                    alert.show();
                 }
-            }
-            else{
-                Alert alert = new Alert(Alert.AlertType.ERROR, "Вы не ввели логин или пароль");
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "В данный момент серверы приложения недоступны! Повторите попытку позже");
                 alert.getDialogPane().getChildren().stream().filter(node -> node instanceof Label).forEach(node -> ((Label)node).setMinHeight(Region.USE_PREF_SIZE));
                 alert.show();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
             }
         });
+
         SignUpBtn.setOnAction(event -> {
             Stage stage = (Stage) SignUpBtn.getScene().getWindow();
             stage.close();
